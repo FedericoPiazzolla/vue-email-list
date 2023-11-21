5478a287d5cb8d3b5518b0674e059bf3f92f9c21
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-21 16:55:08 +0100

    add: function for generate mail in vue js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      arrayMail: [] ,
+      numberEmail: "" ,
+      inputNumber: "" ,
+    }
+  },
+  methods: {
+    genRndEmail: function () {
+      this.arrayMail = [];
+      this.numberEmail = this.inputNumber;
+
+      for (let i = 0; i < this.numberEmail; i++) {
+        axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((resp) => {
+          this.arrayMail.push(resp.data.response)
+        })
+      }
+      
+    },
+  },
+}).mount("#app");
